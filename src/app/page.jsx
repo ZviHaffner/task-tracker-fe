e75a0a47dd3f2886d2c0a5bee6739ffa824389a3
Zvi Haffner <zvihaffner@gmail.com>
2025-04-30 16:00:12 +0100

    Adds functionality to create new tasks with form handling

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { getAllTasks, updateTaskStatus } from "@/api";
+import { addNewTask, getAllTasks, updateTaskStatus } from "@/api";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -8,6 +8,13 @@ export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState("");
   const [editingTaskId, setEditingTaskId] = useState(null);
+  const [showNewTaskForm, setShowNewTaskForm] = useState(false);
+  const [newTask, setNewTask] = useState({
+    title: "",
+    description: "",
+    status: "",
+    due_date: "",
+  });
 
   useEffect(() => {
     getAllTasks()
@@ -51,6 +58,31 @@ export default function Home() {
       className: "bg-green-500 text-white px-3 py-2 rounded",
     },
   };
+
+  function handleNewTaskChange(e) {
+    const { id, value } = e.target;
+
+    setNewTask((prev) => ({ ...prev, [id]: value }));
+  }
+
+  function handleNewTaskSubmit(e) {
+    e.preventDefault();
+
+    addNewTask(newTask)
+      .then((res) => {
+        setTasks((prevTasks) => [...prevTasks, res.data.task]);
+        setNewTask({
+          title: "",
+          description: "",
+          status: "pending",
+          due_date: "",
+        });
+        setShowNewTaskForm(false);
+      })
+      .catch(() => {
+        alert("Failed to add new task.");
+      });
+  }
 
   return (
     <main className="min-h-screen bg-gray-100 p-8">
@@ -127,11 +159,11 @@ export default function Home() {
                             <>
                               <span
                                 className={`${
-                                  statusMap[task.status].className ||
+                                  statusMap[task.status]?.className ||
                                   "bg-gray-300 text-black px-2 py-1 rounded"
                                 }`}
                               >
-                                {statusMap[task.status].text || task.status}
+                                {statusMap[task.status]?.text || task.status}
                               </span>
                               <button
                                 aria-label="Edit task"
@@ -148,6 +180,80 @@ export default function Home() {
                         </td>
                       </tr>
                     ))}
+                    {showNewTaskForm ? (
+                      <>
+                        <tr className="odd:bg-white even:bg-gray-100 hover:bg-gray-50">
+                          <td>
+                            <input
+                              id="title"
+                              className="w-full p-4"
+                              placeholder="Insert Title"
+                              value={newTask.title}
+                              onChange={handleNewTaskChange}
+                            />
+                            {console.log(newTask)}
+                          </td>
+                          <td>
+                            <input
+                              id="description"
+                              className="w-full p-4"
+                              placeholder="Insert Description"
+                              value={newTask.description}
+                              onChange={handleNewTaskChange}
+                            />
+                          </td>
+                          <td>
+                            <select
+                              id="status"
+                              className="w-full p-4"
+                              value={newTask.status}
+                              onChange={handleNewTaskChange}
+                            >
+                              <option value="pending">Pending</option>
+                              <option value="in-progress">In Progress</option>
+                              <option value="completed">Completed</option>
+                            </select>
+                          </td>
+                          <td>
+                            <input
+                              id="due_date"
+                              type="datetime-local"
+                              className="w-full p-4"
+                              value={newTask.due_date}
+                              onChange={handleNewTaskChange}
+                            />
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>
+                            <button
+                              className="my-2 px-3 py-2 rounded bg-blue-500 text-white font-bold hover:bg-blue-100 hover:text-blue-500 cursor-pointer"
+                              type="submit"
+                              onClick={handleNewTaskSubmit}
+                            >
+                              Add Task
+                            </button>
+                            <button
+                              className="my-2 ml-4 px-3 py-2 rounded bg-red-500 text-white font-bold hover:bg-red-100 hover:text-red-500 cursor-pointer"
+                              onClick={() => setShowNewTaskForm(false)}
+                            >
+                              Close
+                            </button>
+                          </td>
+                        </tr>
+                      </>
+                    ) : (
+                      <tr>
+                        <td>
+                          <button
+                            className="my-2 px-3 py-2 rounded bg-blue-500 text-white font-bold hover:bg-blue-100 hover:text-blue-500 cursor-pointer"
+                            onClick={() => setShowNewTaskForm(!showNewTaskForm)}
+                          >
+                            Add Task
+                          </button>
+                        </td>
+                      </tr>
+                    )}
                   </tbody>
                 </table>
               </div>
